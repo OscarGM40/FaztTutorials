ffaebab804740af99a2fc4b21d3a82bb01236960
Oscar <oscargm28@hotmail.com>
2022-01-15 19:14:13 +0100

    init repo

---
+++ graphql-mysql-fazt/src/index.ts
@@ -0,0 +1,19 @@
+import { app } from "./app";
+import { connectDB } from './db';
+
+
+
+/*  */
+(async function main(){
+  try {
+    await connectDB();
+    app.listen(3000, () => {
+      console.clear();
+      console.log('Listening on port 3000');
+    });
+  }catch (error){
+    console.log(error);
+  }
+})();
+
+
